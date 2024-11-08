804f106c691ff6088d58e2cf626524cd39cb6935
ahmad farachi <103252037+ahmadfa-lb@users.noreply.github.com>
2024-11-08 19:02:05 +0200

    adding an Undo button

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,18 @@ import SudokuGrid from './components/SudokuGrid';
 import NumberButtons from './components/NumbersBtns';
 import './App.css';
 
+// src/App.tsx or src/fontAwesome.ts
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+
+library.add(fas, fab); // Add solid and brand icons to the library
+
+
 const App: React.FC = () => {
   const [grid, setGrid] = useState(Array.from({ length: 9 }, () => Array(9).fill('')));
   const [focusedCell, setFocusedCell] = useState<{ row: number; col: number } | null>(null);
@@ -16,7 +28,7 @@ const App: React.FC = () => {
   return (
     <div className='game-container'>
       <h1>Soduko Game</h1>
-      <div className='mistakes'>Mistakes: <span>0/6</span></div>
+      <div className='mistakes'>Mistakes: <span>0/3</span></div>
       <SudokuGrid 
         focusedCell={focusedCell} 
         setFocusedCell={setFocusedCell} 
@@ -24,6 +36,7 @@ const App: React.FC = () => {
         grid={grid}
         setGrid={setGrid}
       />
+      <button className='undo-btn'><FontAwesomeIcon icon="fa-solid fa-arrow-rotate-left" /></button>
       <NumberButtons onNumberClick={handleNumberClick} />
     </div>
   );
