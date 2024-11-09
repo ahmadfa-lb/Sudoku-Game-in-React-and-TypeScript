16d182a6efd68df2b4f289ff382c02264569b55a
ahmad farachi <103252037+ahmadfa-lb@users.noreply.github.com>
2024-11-09 23:25:02 +0200

    styling the play-again btn

--- src/components/BoxResult.tsx
+++ src/components/BoxResult.tsx
@@ -1,26 +1,3 @@
-// import React from 'react';
-// import '../index.css'
-// import lostGif from '../assets/lost.gif';
-
-
-// interface BoxResultProps {
-//     resetGame: () => void; // Accept resetGame as a prop
-//   }
-
-//   const BoxResult: React.FC<BoxResultProps> = ({ resetGame }) => {
-//     return (
-//       <div className="game-modal show">
-//         <div className="content">
-//           <img src={lostGif} alt="Game over" />
-//           <button className="play-again" onClick={resetGame}>Play Again</button>
-//         </div>
-//       </div>
-//     );
-//   };
-
-// export default BoxResult;
-
-// BoxResult.tsx (No changes needed from previous)
 import React from 'react';
 import '../index.css';
 import lostGif from '../assets/lost.gif';
